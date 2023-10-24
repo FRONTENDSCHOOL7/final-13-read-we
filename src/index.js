2cdb9b0253ae6aafd2f62ebc72991248af463654
TheVolunteers <achbeliever@gmailcom>
2023-10-24 11:21:55 +0900

    design : 컴포넌트 html/css 추가

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ComponentEx from './ComponentEx';
 import './common.css';
+import Header from './Header';
+import './App.css';
+import Footer from './Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <ComponentEx />
+    <Header></Header>
+    <Footer></Footer>
   </React.StrictMode>,
 );
