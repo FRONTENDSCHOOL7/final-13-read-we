352fcfae6fc9d4c02576144d27b443a764d6f009
dongmay <sdh4202@naver.com>
2023-10-31 09:18:05 +0900

    Feat: Mainpage index, navbtn router추가

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './common.css';
 import App from './App';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
