a1bd09d3418ddc2422fb4661dc47f035410d2aff
seungmimi <seungmimi10@gmail.com>
2023-11-06 14:41:58 +0900

    Doc: 인덱스 경로 수정

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import { useState } from 'react';
 import LoginPage from './login-Page/LoginPage';
 import MainPage from './components/main/MainPage';
 import { Routes, Route, Link } from 'react-router-dom';
-import CmtList from './main-page/CmtList';
 import MyPage from './myPage/MyPage';
 import YourPage from './myPage/YourPage';
 import MyPageAccList from './myPage/MyPageAccList';
 import YourPageAccList from './myPage/YourPageAccList';
 import MyLibrary from './myPage/MyLibrary';
 import JoinPage from './join-Page/JoinPage';
-import Emailsignup from './login-Page/Emailsignup';
 import ProfileEdit from './myPage/ProfileEdit';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
@@ -24,10 +22,10 @@ function App() {
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<LoginJoinSelect />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/join" element={<JoinPage />} />
         <Route path="/join-success" element={<LoginFinish />} />
-        <Route path="/email-signup" element={<Emailsignup />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/postupload" element={<PostUpload />} />
         {/* <Route path="/comment" element={<CmtList />} /> */}
@@ -39,7 +37,6 @@ function App() {
         <Route path="/yourpage" element={<YourPage />} />
         <Route path="/yourpage/youracclist" element={<YourPageAccList />} />
         <Route path="/profile-edit" element={<ProfileEdit />} />
-        <Route path="/select" element={<LoginJoinSelect />} />
         <Route path="*" element={<div>404 Page</div>} /> {/*  404page */}
         <Route path="/search" element={<SearchModal />} />
       </Routes>
