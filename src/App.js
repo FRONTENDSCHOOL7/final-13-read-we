b9004b2aee45ca9586d26e5bf4746114b9576851
ach <achbeliever@gmail.com>
2023-11-06 10:51:11 +0900

    Fix : join  오류수정

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/join" element={<JoinPage />} />
         <Route path="/join-success" element={<LoginFinish />} />
         <Route path="/email-signup" element={<Emailsignup />} />
