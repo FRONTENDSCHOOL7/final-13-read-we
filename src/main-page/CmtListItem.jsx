cb05628ce0a54e34728b219ae4a6bc24cc338d17
obersee <dgwc110@gmail.com>
2023-11-07 13:55:54 +0900

    Fix: 댓글 프로필 이미지 경로 수정

--- src/main-page/CmtListItem.jsx
+++ src/main-page/CmtListItem.jsx
@@ -6,7 +6,13 @@ const CmtListItem = ({ cmtItem }) => {
     <li className={styles.cmtList}>
       <div className={styles.cmtImgText}>
         <div className={styles.cmtImg}>
-          <img src={cmtItem.author.image} alt="프로필" />
+          <img
+            src={
+              'https://api.mandarin.weniv.co.kr/' +
+              cmtItem.author.image.replace(/^.*\//, '')
+            }
+            alt="프로필"
+          />
         </div>
         <div className={styles.cmtText}>
           <strong>{cmtItem.author.username}</strong>
