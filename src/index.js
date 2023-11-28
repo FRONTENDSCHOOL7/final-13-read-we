5c59919f26e018ebb31832416af4b555caf08cff
seungmimi <seungmimi10@gmail.com>
2023-11-28 17:32:50 +0900

    Docs: App.css삭제

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './common.css';
 import App from './App';
-import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
