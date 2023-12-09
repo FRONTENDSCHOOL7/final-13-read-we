4a9a416bf1b0399cc3a775a8380c2ba18225fc4c
dongmay <sdh4202@naver.com>
2023-12-09 15:08:55 +0900

    Feat,Docs-Re: 책검색 기능, 검색결과 case기능 추가

--- src/pages/main/SearchModal.jsx
+++ src/pages/main/SearchModal.jsx
@@ -1,27 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import styles from './css/SearchModal.module.css';
 import axios from 'axios';
-import { IconIpt, BtnIpt } from '../../components/input/IptStyleEtc';
+import { IconIpt } from '../../components/input/IptStyleEtc';
 import { BasicIpt } from '../../components/input/IptStyle';
 import { BasicBtn } from '../../components/button/BtnStyle';
+import { SearchIpt } from '../../components/input/IptStyleEtc';
 
 const SearchModal = ({ hideModal }) => {
   const [searchBook, setSearchBook] = useState([]); // 책검색 결과
   const [bookName, setBookName] = useState(null); // 책제목
+  const [searchResult, setSearchResult] = useState(true); // 검색결과 상태
 
+  // 팝업 열렸을 경우 body스크롤 방지
   useEffect(() => {
-    console.log(searchBook);
-  }, [searchBook]);
+    document.body.style.cssText = `
+          position: fixed; 
+          top: -${window.scrollY}px;
+          overflow-y: scroll;
+          width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
 
   // 알라딘 책검색
-  const fetchBook = async () => {
+  const fetchBook = async (e) => {
+    e.preventDefault();
+    //검색어가 비어있을 경우
+    if (bookName === null || bookName === '') {
+      alert('검색어를 입력해주세요.');
+      return;
+    }
+
+    console.log('검색버튼누름');
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const aladinUrl = `http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttb22pqpq1534001&Query=${bookName}&QueryType=Title&MaxResults=5&start=1&SearchTarget=Book&output=JS&Version=20131101`;
+    const aladinUrl = `http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttb22pqpq1534001&Query=${bookName}&QueryType=Title&MaxResults=50&start=1&SearchTarget=Book&Book&Cover=Big&output=JS&Version=20131101`;
 
     try {
       const response = await axios.get(proxyUrl + aladinUrl);
       if (response.status === 200) {
         setSearchBook(response.data.item); // API 호출 결과를 searchBook state에 저장
+        setSearchResult(bookName);
       } else {
         console.log('Error: ', response.status);
       }
@@ -31,14 +52,33 @@ const SearchModal = ({ hideModal }) => {
   };
 
   const DisplayBooks = () => {
+    // 따로 컴포넌트 처리하기.
     return searchBook.map((book, index) => {
       return (
-        <div key={index}>
-          <h3>{book.title}</h3>
-          <p>{book.author}</p>
-          <img src={book.cover} alt={book.title} />
-          <p>{book.description}</p>
-          <a href={book.link}>자세히 보기</a>
+        // eslint-disable-next-line react/jsx-key
+        <div className="modal-content row-2">
+          <div className="row-2">
+            <div className="book-info-obj">
+              <img alt="책 이미지" src={book.cover} />
+            </div>
+            <div className="book-info-text">
+              <div className={styles.disbooks_flexwrap}>
+                <strong className="book-public">
+                  <span>출판사 |</span> {book.publisher}
+                </strong>
+                <a className={styles.disbooks_link} href={book.link}>
+                  구매하기(알라딘) &gt;
+                </a>
+              </div>
+              <h3 className="book-title">{book.title}</h3>
+              <div>
+                <span className="tag orange">지은이</span>
+                <strong className="book-author">{book.author}</strong>
+              </div>
+              <p className="book-content ellipsis">{book.description}</p>
+            </div>
+            <p className="end-point" />
+          </div>
         </div>
       );
     });
@@ -46,33 +86,27 @@ const SearchModal = ({ hideModal }) => {
 
   return (
     <div className={styles.pageWrap}>
+      <button className={styles.closeBtn} onClick={hideModal}>
+        <i className="icon icon-xbtn" />
+      </button>
       <div className={styles.contentArea}>
-        <BtnIpt>
-          <BasicIpt placeholder="내용을 입력해 주세요" />
-          <BasicBtn>등록</BasicBtn>
-        </BtnIpt>
-        <IconIpt>
-          <BasicIpt
-            sm="true"
-            placeholder="검색"
-            type="text"
-            value={bookName}
-            onChange={(e) => setBookName(e.target.value)}
-          />
-          <i class="icon icon-search" />
-          <button
-            onClick={bookName !== null && bookName !== '' ? fetchBook : null}
-          >
-            검색
-          </button>
-        </IconIpt>
-        {searchBook === undefined ? (
+        <SearchIpt>
+          <IconIpt>
+            <BasicIpt
+              sm="true"
+              placeholder="검색어를 입력해 주세요"
+              type="text"
+              value={bookName}
+              onChange={(e) => setBookName(e.target.value)}
+            />
+            <i class="icon icon-search" />
+          </IconIpt>
+          <BasicBtn onClick={fetchBook}>검색</BasicBtn>
+        </SearchIpt>
+
+        {searchBook.length === 0 ? (
           <>
-            <div className={styles.header}>
-              <button className={styles['more-show']} onClick={hideModal}>
-                취소
-              </button>
-            </div>
+            <div className={styles.header} />
             <div className={styles['example-books']}>
               <div>
                 <img
@@ -105,7 +139,7 @@ const SearchModal = ({ hideModal }) => {
             </div>
             <div className={styles['search-result']}>
               <h3 className={styles['show-result']}>
-                <span className={styles.emphasis}>'멋사 프론트엔드'</span>에
+                <span className={styles.emphasis}>{searchResult}</span>에 대한
                 대한 검색 결과
               </h3>
               <div className={styles.zero}>
@@ -113,7 +147,7 @@ const SearchModal = ({ hideModal }) => {
               </div>
               <div className={styles['center-container']}>
                 <p className={styles['no-result']}>
-                  '멋사 프론트엔드'에 대한 검색 결과가 없습니다.
+                  {searchResult}에 대한 검색 결과가 없습니다.
                 </p>
                 <div className={styles.check}>
                   <p>
@@ -125,13 +159,14 @@ const SearchModal = ({ hideModal }) => {
                 </div>
               </div>
             </div>
-          </> // 검색 결과가 있을 때 보여줄 요소들
+          </>
         ) : (
-          <div>
-            <button className={styles['more-show']} onClick={hideModal}>
-              취소
-            </button>
-            <h3>검색 결과</h3>
+          // 검색 결과가 있을 때 보여줄 요소들
+          <div className={styles.disbooks_list}>
+            <h3 className={styles['show-result']}>
+              <span className={styles.emphasis}>{searchResult}</span>에 대한
+              대한 검색 결과
+            </h3>
             <DisplayBooks />
           </div>
         )}
