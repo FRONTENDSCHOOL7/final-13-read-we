fff488e000ae3343a9ae350d8514eb98f0e40974
seungmimi <seungmimi10@gmail.com>
2023-12-10 16:47:08 +0900

    Style-Re: [공통]로딩중 컴포넌트 추가

---
+++ src/components/popup/LoadingModal.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './css/LoadingModal.module.css';
+
+const LoadingModal = () => {
+  return <div className={styles.loading} />;
+};
+
+export default LoadingModal;
