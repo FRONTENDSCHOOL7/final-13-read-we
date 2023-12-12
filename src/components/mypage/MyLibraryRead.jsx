5583f99a67dc08ba9a01316e6e6b29af92702bd8
seungmimi <seungmimi10@gmail.com>
2023-12-12 14:29:24 +0900

    Docs-Re: [내서재]이미지경로수정(로컬, 알라딘 URL case구분)

--- src/components/mypage/MyLibraryRead.jsx
+++ src/components/mypage/MyLibraryRead.jsx
@@ -5,7 +5,15 @@ const MyLibraryRead = (props) => {
   return (
     <li className={styles.mybookListObj}>
       <div className={styles.mybookListObjImg}>
-        <img src={process.env.PUBLIC_URL + `/images/${props.bookImgSrc}`} />
+        <img
+          alt="책 이미지"
+          // 알라딘 API로 받아온 이미지와 로컬 이미지 url case구분
+          src={
+            /aladin/i.test(props.bookImgSrc)
+              ? props.bookImgSrc
+              : `/images/${props.bookImgSrc}`
+          }
+        />
       </div>
       <p>{props.title}</p>
     </li>
